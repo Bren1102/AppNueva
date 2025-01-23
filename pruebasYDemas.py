e515325a7a4d0674357645d84f4eab7aa41dc28b
Bren1102 <bmorrone@hotmail.com>
2025-01-23 19:37:36 -0300

    creacion entorno virtual

--- pruebasYDemas.py
+++ pruebasYDemas.py
@@ -5,4 +5,7 @@ import pip
 pip.main(['install', 'PyPDF2'])
 
 
+#consultar error
+#python -m venv env
+#source env/bin/activate
 
---
+++ static/Remito.py
@@ -0,0 +1,7 @@
+class Remito:
+    def __init__(self, archivo):
+        self.archivo = archivo
+
+    def cargar_remito(self):
+        # Lógica para cargar el remito (PDF o JPG)
+        pass
